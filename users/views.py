238b456f9696266b5f2153303ef3f4d4cb6818bb
Полина <farutina97@gmail.com>
2025-06-09 20:27:10 +0300

    test

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from rest_framework.generics import CreateAPIView
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserRegistrationView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = []
+
+
